5740dcb2f5c141d842d18345a6d887448a06ce96
Karoluks <karolis.malisauskas@gmail.com>
2017-09-27 02:20:37 +0300

    ad

--- models/connection.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const connect = mongoose.connect('mongodb://karkia:karookia@ds143744.mlab.com:43744/learningnode', {
-    useMongoClient: true,
-
-});
-
-module.exports = connect;
